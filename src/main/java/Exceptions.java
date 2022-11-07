876aeabc791512141dd877a485f4d6a7f9f61eec
G00403720 <G00403720@atu.ie>
2022-11-07 10:40:13 +0000

    Looping with exceptions

--- src/main/java/Exceptions.java
+++ src/main/java/Exceptions.java
@@ -4,11 +4,12 @@ import java.util.InputMismatchException;
 
 public class Exceptions {
     public static void main(String[] args) {
+        int x = 1;
         try{
             System.out.println("Enter a number between 0 - 9 ");
             Scanner newscan = new Scanner(System.in);
             int No = newscan.nextInt();
-            if(No <= 9 && No > 0){
+            if(No <= 9 && No >= 0){
                 System.out.println("You entered: " + No);
             }
             else{
@@ -17,6 +18,18 @@ public class Exceptions {
         }
         catch(InputMismatchException a){
             System.out.println("Enter a vaild number");
+            while (x > 0){
+                System.out.println("Enter a number between 0 - 9 ");
+                Scanner newscan = new Scanner(System.in);
+                int No2 = newscan.nextInt();
+                if(No2 <= 9 && No2 > 0){
+                    System.out.println("You entered: " + No2);
+                }
+                else{
+                    System.out.println("Not a valid number: " + No2);
+                }
+                break;
+            }
         }
     }
 
