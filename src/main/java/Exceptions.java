d9f7acaeb3275dac12ff214b0d997ea49fe6df52
G00403720 <G00403720@atu.ie>
2022-11-07 09:55:40 +0000

    catching a char exception

--- src/main/java/Exceptions.java
+++ src/main/java/Exceptions.java
@@ -1,17 +1,22 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Exceptions {
     public static void main(String[] args) {
-        System.out.println("Enter a number between 0 - 9 ");
-        Scanner newscan = new Scanner(System.in);
-        int No = newscan.nextInt();
-
-        if(No <= 9 || No > 0){
-            System.out.println("You entered: " + No);
+        try{
+            System.out.println("Enter a number between 0 - 9 ");
+            Scanner newscan = new Scanner(System.in);
+            int No = newscan.nextInt();
+            if(No <= 9 && No > 0){
+                System.out.println("You entered: " + No);
+            }
+            else{
+                System.out.println("Not a valid number: " + No);
+            }
         }
-        else{
-            System.out.println("Not a valid number");
+        catch(InputMismatchException a){
+            System.out.println("Enter a vaild number");
         }
     }
 
